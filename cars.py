926b50c03a0e0918695b91755328c01750f9a266
yzhou <charles.zhou.6006@gmail.com>
2019-08-22 12:57:58 -0700

    the requests request can't be understood by server restful

--- cars.py
+++ cars.py
@@ -1,26 +1,45 @@
 import argparse
 
 from src import cache
+from src.utils import detect_car_give_ts
 
 parser = argparse.ArgumentParser()
-parser.add_argument("detect", nargs='*', default=None, )
+parser.add_argument("args", nargs='*', default=None, )
 
 # parser.add_argument('')
 args = parser.parse_args()
-args_len = len(args.detect)
+args_len = len(args.args)
 
-if args_len < 1:
+if args_len < 2:
     print('''
-    Usage:
-    1.
-    detect [int]timestamp , detect if given time stamp is a car
-    usage:
-    python cars.py detect 1538076003
-    2.
-    compare [int]timestamp1, [int]timestamp2
-    ''')
+Usage:
+1. detect [int]timestamp 
+    - detect if given time stamp is a car
+usage:
+python cars.py detect 1538076003
+2. compare [int]timestamp1, [int]timestamp2
+    - check if two cars are the same
+usage:
+python cars.py compare 1538076003 1538079781
+3. analyze [int]timestamp1, [int]timestamp2
+    - produce report between two timestamps
+usage:
+python cars.py analyze 1538076003 1538079781
+''')
+    exit()
 
 
-commend, others = args.detect
+commend = args.args[0]
+if commend.lower() == 'detect':
+    ts = args.args[1]
+    ts = int(ts)
+    print(detect_car_give_ts(ts))
+    pass
+elif commend.lower() == 'compare':
+    pass
+elif commend.lower() == 'analyze':
+    pass
+else:
+    print('unknown commend %s' % (commend))
+    exit()
 
-print(commend, others)
--- src/client_config.py
+++ src/client_config.py
@@ -1,1 +1,13 @@
 server_ip = 'http://136.25.41.218:5000/'
+
+GET_IMAGE_ENDPOINT_PREFIX = 'http://136.25.41.218:5000/v1/ask/get_image?ts='
+
+IS_SAME_CAR_ENDPOINT_PREFIX = 'http://136.25.41.218:5000/v1/ask/match?targetObject=car&returnType=json&pValue=10&'
+
+DETECT_CAR_ENDPOINT_PREFIX = 'http://136.25.41.218:5000/v1/ask/contain?returnType=json&timeStamp=1'
+
+GET_FILE_ENDPOINT_PREFIX = 'http://136.25.41.218:5000/v1/ask/get_file?ts='
+
+
+
+
