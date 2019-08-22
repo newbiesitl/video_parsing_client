bd7203f3345d052699801638cde873363ed3d518
yzhou <charles.zhou.6006@gmail.com>
2019-08-22 12:32:43 -0700

    add arg parser

--- cars.py
+++ cars.py
@@ -3,9 +3,24 @@ import argparse
 from src import cache
 
 parser = argparse.ArgumentParser()
-parser.add_argument("detect", help="detect car of a given time stamp",
-                    type=int)
+parser.add_argument("detect", nargs='*', default=None, )
 
-
+# parser.add_argument('')
 args = parser.parse_args()
-ts = args.detect
+args_len = len(args.detect)
+
+if args_len < 1:
+    print('''
+    Usage:
+    1.
+    detect [int]timestamp , detect if given time stamp is a car
+    usage:
+    python cars.py detect 1538076003
+    2.
+    compare [int]timestamp1, [int]timestamp2
+    ''')
+
+
+commend, others = args.detect
+
+print(commend, others)
